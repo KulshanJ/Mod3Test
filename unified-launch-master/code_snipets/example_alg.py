bd5418779933c042fa44c9839debc433af20c456
srigs97 <100980299+srigs97@users.noreply.github.com>
2022-03-23 13:08:29 -0400

    Update example_alg.py

--- unified-launch-master/code_snipets/example_alg.py
+++ unified-launch-master/code_snipets/example_alg.py
@@ -19,8 +19,11 @@ def main():
                 #print("TOO CLOSE")
         rover.send_command(left_side_speed, right_side_speed)
         i = i + 1
-        sleep(0.01)
+        time.sleep(500)
         
+    left_side_speed = 7
+    right_side_speed = -7
+    time.sleep(5)
         
 if __name__ == "__main__":
     main()
