bfc43c65962bd911ec36560678a42c62721f6bcc
KulshanJ <100979345+KulshanJ@users.noreply.github.com>
2022-03-25 12:44:52 -0400

    Update example_alg2.py

--- unified-launch-master/code_snipets/example_alg2.py
+++ unified-launch-master/code_snipets/example_alg2.py
@@ -1,7 +1,7 @@
 from qset_lib import Rover
 from time import sleep
 
-'''
+
 def main():
     
     rover = Rover()
@@ -9,15 +9,15 @@ def main():
     i = 0
 
     left_side_speed = 7
-    right_side_speed = -7
+    right_side_speed = 7
 
     while i < 3000:
         #print("X: " + rover.x + " Y: " + rover.y + " Heading: " + rover.heading)
 
         for dist in rover.laser_distances:
             if dist < 0.5:
-                    left_side_speed = 0
-                    right_side_speed = 0
+                    left_side_speed = -7
+                    right_side_speed = -7
                 #print("TOO CLOSE")
         rover.send_command(left_side_speed, right_side_speed)
         i = i + 1
