3b2940d5b712527c345dda6adb9f570d5f25a4bb
srigs97 <100980299+srigs97@users.noreply.github.com>
2022-03-25 12:06:08 -0400

    Update example_alg.py

--- unified-launch-master/code_snipets/example_alg.py
+++ unified-launch-master/code_snipets/example_alg.py
@@ -1,12 +1,12 @@
 from qset_lib import Rover
 from time import sleep
-'''
+
     rover = Rover()
     
     i = 0
 
-    left_side_speed = 7
-    right_side_speed = 7
+    left_side_speed = -7
+    right_side_speed = -7
 
     while i < 3000:
         #print("X: " + rover.x + " Y: " + rover.y + " Heading: " + rover.heading)
@@ -23,8 +23,9 @@ from time import sleep
 
 if __name__ == "__main__":
     main()
- '''
+ 
 
+'''
 rover = Rover()
 
 i = 0
@@ -50,3 +51,5 @@ while i < 10:
         rover.send_command(left_side_speed, right_side_speed)
 
         i = i + 1 
+       
+       '''
