c86a427850c353b3fa8ed690ce33a3682d09ba67
SeanRen01 <101027861+SeanRen01@users.noreply.github.com>
2022-03-26 09:01:11 -0400

    Update example_alg2.py

--- unified-launch-master/code_snipets/example_alg2.py
+++ unified-launch-master/code_snipets/example_alg2.py
@@ -1,6 +1,163 @@
 from qset_lib import Rover
 from time import sleep
 
+import math
+import numpy as np
+
+
+def angleFinding(previousPosition, currentPosition):
+    '''
+    Previous ---> tail of the vector
+    Current ---> head of the vector
+    '''
+    reference = np.array([0,1])
+
+    '''
+    Finding the heading vector
+    '''
+    m = np.array(previousPosition)
+    n = np.array(currentPosition)
+    vector = np.subtract(n,m)
+
+
+    '''
+    According to vector dot product rule, calculate the angle between the vector and the reference vector
+    '''
+    products = np.dot(vector, reference)
+    cosX = products / (np.linalg.norm(vector))
+    angle = math.acos(cosX)
+
+    '''
+    We want to track the angle from the +y direction to the vector, 
+    but the angle returned is always the smaller one
+    So we check here to see if the vector is in 1/4 quadrant or 2/3 quadrant
+    '''
+    checkingVector = np.array([1,0])
+    check = np.dot(checkingVector,vector)
+
+    '''
+    Convert the small angle in 2/3 quadrant to the big angle we want
+    '''
+    if check < 0:
+        angle= 2* math.pi - angle
+
+    return angle
+
+
+
+def pathDecision(currentAngle, desiredAngle):
+    angleToTurn = desiredAngle - currentAngle
+    return angleToTurn
+
+
+
+
+def turning (angleToTurn):
+    if angleToTurn < 0:
+        '''Turning counter clockwise here'''
+    else:
+        '''Turning clockwise here'''
+
+
+
+'''
+Calculating the alert distance for each of the 32 'lines'
+Set the list as global
+'''
+roverWidth = 0.61
+listOfDistance1 = [roverWidth / math.sin(math.pi/4)]
+i = 2
+deltaAngle = math.pi/(2*31)
+while i <= 32:
+    listOfDistance1.append(roverWidth / math.sin((math.pi/4) + (deltaAngle*(i-1))))
+    i += 1
+
+
+
+roverRange = math.sqrt(0.61**2 + 0.61 ** 2)
+listOfDistance2 = [roverRange / math.sin(math.pi/4)]
+i = 2
+while i <= 32:
+    listOfDistance2.append(roverRange / math.sin((math.pi/4) + (deltaAngle*(i-1))))
+    i += 1
+
+
+
+
+
+def distanceChecking1(listOfAlertDistance1, listFromLiDAR):
+    '''
+    Calculate the delta distance and will return a list containing the difference
+    '''
+
+    listOfDifference = np.array(np.array(listFromLiDAR) - np.array(listOfAlertDistance1)).tolist()
+
+    '''
+        Return false if the path is clear ---> keep moving straightforward
+        return true if the obstacle is within its path ---> need further actions
+    '''
+    return listOfDifference, any(n < 0 for n in listOfDifference)
+
+'''
+flag will be defined in main function
+the value will be set to default(0) after every stop and scan
+'''
+def distanceChecking2(listOfAlertDistance2, listFromLiDAR, flag):
+    '''
+    Calculate the delta distance and will return a list containing the difference
+    '''
+    listOfDifference = np.array(np.array(listFromLiDAR) - np.array(listOfAlertDistance2)).tolist()
+
+    if any(n < 0 for n in listOfDifference) == True:
+        '''means something is in the turning radius'''
+        flag = flag + 1
+    else:
+        flag = 0
+    '''
+    If the flag is 1, indicating something is within the turning radius of the rover.
+    
+    If the flag is over 0, means the rover has moved forward under the condition that there's not enough space for 
+    turning. In this case, the rover may already be stuck.
+    
+    
+    '''
+    return flag
+
+'''
+flag == 1 && true : something is in the path as well as the turning radius ---> stop and scan
+flag == 1 && false: path is clear but the rover may not be able to turn ---> keep moving forward
+flag > 1 && true: stuck ---> turn into Lucas's function, which is moving back and scaning
+flag == 0 && true: something in the path ---> stop and scan
+flag == 0 && false: all is clear ---> move forward
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def main():
     
