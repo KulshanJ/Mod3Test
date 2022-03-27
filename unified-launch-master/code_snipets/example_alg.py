7ab379f206403558e4a33ebd509b5c790a78ff42
KulshanJ <100979345+KulshanJ@users.noreply.github.com>
2022-03-27 18:24:16 -0400

    Update example_alg.py

--- unified-launch-master/code_snipets/example_alg.py
+++ unified-launch-master/code_snipets/example_alg.py
@@ -11,7 +11,7 @@ def main():
     left_side_speed = 7
     right_side_speed = 7
     
-    __modelstates_callback(self, msg)
+    heading = rover.heading
 
     while i < 3000:
         #print("X: " + rover.x + " Y: " + rover.y + " Heading: " + rover.heading)
