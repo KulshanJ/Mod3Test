172b4b1946be8cfc5b3209b143895103d46798ad
KulshanJ <100979345+KulshanJ@users.noreply.github.com>
2022-03-27 18:06:40 -0400

    Update example_alg.py

--- unified-launch-master/code_snipets/example_alg.py
+++ unified-launch-master/code_snipets/example_alg.py
@@ -18,7 +18,7 @@ def main():
             if dist < 0.5:
                     left_side_speed = -7
                     right_side_speed = -7
-                #print("TOO CLOSE")
+                print(self.heading)
         rover.send_command(left_side_speed, right_side_speed)
         i = i + 1
         sleep(0.01)
