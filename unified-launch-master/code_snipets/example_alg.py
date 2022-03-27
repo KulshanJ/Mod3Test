0274894101363aac82a910bf6c6fb428107a3417
KulshanJ <100979345+KulshanJ@users.noreply.github.com>
2022-03-27 19:12:40 -0400

    Update example_alg.py

--- unified-launch-master/code_snipets/example_alg.py
+++ unified-launch-master/code_snipets/example_alg.py
@@ -10,14 +10,14 @@ def main():
     left_side_speed = 2
     right_side_speed = -2
 
-    while i < 1000:
-        
+    while i < 10:
+        print("X: " + str(rover.x) + " Y: " + str(rover.y) + " Heading: " + str(rover.heading))
 
         for dist in rover.laser_distances:
             if dist < 2:
                     left_side_speed = 2
                     right_side_speed = -2
-                    print("X: " + str(rover.x) + " Y: " + str(rover.y) + " Heading: " + str(rover.heading))
+                    
                 
         rover.send_command(left_side_speed, right_side_speed)
         i = i + 1
