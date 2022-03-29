bb0a95c52625779b32f5c3f5e9cdc29d166aa2a5
SeanRen01 <101027861+SeanRen01@users.noreply.github.com>
2022-03-29 17:36:45 -0400

    Update example_alg.py

--- unified-launch-master/code_snipets/example_alg.py
+++ unified-launch-master/code_snipets/example_alg.py
@@ -10,7 +10,7 @@ def main():
     left_side_speed = 7
     right_side_speed = 7
 
-    while i < 3000:
+    while i < 1000:
         #print("X: " + str(rover.x) + " Y: " + str(rover.y) + " Heading: " + str(rover.heading))
 
         
