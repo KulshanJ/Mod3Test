7d81b556f36bd32eac602b49d738269bcca1abbe
KulshanJ <100979345+KulshanJ@users.noreply.github.com>
2022-03-29 21:57:17 -0400

    Update example_alg2.py

--- unified-launch-master/code_snipets/example_alg2.py
+++ unified-launch-master/code_snipets/example_alg2.py
@@ -91,7 +91,7 @@ def turningFunction(angle):
     x = desiredHeading - 1
     y = desiredHeading + 1
      
-    if heading < y && x < heading:
+    if heading < y and x < heading:
         left_side_speed = 0
         right_side_speed = 0
         rover.send_command(left_side_speed, right_side_speed)
