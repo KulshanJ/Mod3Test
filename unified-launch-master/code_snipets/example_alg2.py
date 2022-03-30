2ae275de670e1b9deee3bfa6330419d3a877ad95
KulshanJ <100979345+KulshanJ@users.noreply.github.com>
2022-03-30 00:54:59 -0400

    Update example_alg2.py

--- unified-launch-master/code_snipets/example_alg2.py
+++ unified-launch-master/code_snipets/example_alg2.py
@@ -165,8 +165,8 @@ def stop_check(closestDistance, heading):
     # variables for sleep time, speed, stopping distance, minimum tolerated distance
     speed = 1
     sleeptime = 2
-    closestdist = 1
-    mindist = 2
+    closestdist = 1.75
+    mindist = 2.5
 
     # call on lidar function to determine distance from object
     objectdist = closestDistance #not to be confused with closestdist... this calls on Sean's function
