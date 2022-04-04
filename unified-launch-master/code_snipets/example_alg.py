d283591df2e6981b597603df97ea187f23ddf10e
SeanRen01 <101027861+SeanRen01@users.noreply.github.com>
2022-04-04 15:33:46 -0400

    Update example_alg.py

--- unified-launch-master/code_snipets/example_alg.py
+++ unified-launch-master/code_snipets/example_alg.py
@@ -278,8 +278,8 @@ def main():
 
         listOfLiDAR = getLiDARDistance()
         
-        left_side_speed = 0
-        right_side_speed = 0
+        left_side_speed = 10
+        right_side_speed = 10
         rover.send_command(left_side_speed, right_side_speed)
         closestDistance, result = distanceChecking1(listOfLiDAR)
         
