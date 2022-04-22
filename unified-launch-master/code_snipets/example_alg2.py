c1ab7d03e0e355eb4cd9756baebddc93d6529222
KulshanJ <100979345+KulshanJ@users.noreply.github.com>
2022-04-21 22:14:27 -0400

    Update example_alg2.py

--- unified-launch-master/code_snipets/example_alg2.py
+++ unified-launch-master/code_snipets/example_alg2.py
@@ -405,8 +405,8 @@ def main():
     right_side_speed = -5
     rover.send_command(left_side_speed, right_side_speed)
     
-    x = theta - 2
-    y = theta + 2
+    x = theta - 5
+    y = theta + 5
     
     check = 1
     #heading < y and x < heading
@@ -428,24 +428,6 @@ def main():
 
     while i < 3000:
         rover.send_command(left_side_speed, right_side_speed)
-        
-        sleep(0.8)
-        
-        left_side_speed = 5
-        right_side_speed = -5
-        rover.send_command(left_side_speed, right_side_speed)
-        
-        check = 1
-        #heading < y and x < heading
-        while check == 1:
-            realheading = rover.heading
-            print(str(realheading) + "realheading")
-            print(str(theta) + "desiredHeading")
-            if realheading < y and x < realheading:
-                left_side_speed = 0
-                right_side_speed = 0
-                rover.send_command(left_side_speed, right_side_speed)
-                check = 0
         
         #flag = 0
         
